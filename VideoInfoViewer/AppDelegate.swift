7c9b710a58fe73fe4917d025f91c9e5f5cc46f25
Hool, Rory <hoolrory@gmail.com>
2016-09-01 19:11:27 -0400

    Fix dumb warning, explanation of weirdness in comment

--- VideoInfoViewer/AppDelegate.swift
+++ VideoInfoViewer/AppDelegate.swift
@@ -91,18 +91,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         var failureReason = "There was an error creating or loading the application's saved data."
         do {
             try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
-        } catch {
+        } catch let error as NSError {
             // Report any error we got.
             var dict = [String: AnyObject]()
             dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
             dict[NSLocalizedFailureReasonErrorKey] = failureReason
 
-            dict[NSUnderlyingErrorKey] = error as! NSError
+            dict[NSUnderlyingErrorKey] = error
             let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
             // Replace this with code to handle the error appropriately.
             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
             abort()
+        } catch {
+            // dummy via http://stackoverflow.com/questions/34722649/errortype-is-not-convertible-to-nserror
         }
         
         return coordinator
